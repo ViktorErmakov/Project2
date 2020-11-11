a7a5b38b60699b41acae3031482daced7de75196
Viktor Ermakov <79091602201@yandex.ru>
2020-11-11 21:12:40 +0300

    сделал второе домашнее задание

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 const personalMovieDb = {
     count: numberOfFilms,
@@ -7,12 +9,31 @@ const personalMovieDb = {
     privat: false
 };
 
-let Film1 = prompt('Один из последних просмотренных фильмов?', '');
-let star1 = prompt('На сколько оцените его?', '');
-let Film2 = prompt('Один из последних просмотренных фильмов?', '');
-let star2 = prompt('На сколько оцените его?', '');
-
-personalMovieDb.movies[Film1] = star1;
-personalMovieDb.movies[Film2] = star2;
-
-console.log(personalMovieDb);
+for (let i = 1; i < 3; i++) {
+    
+    const film = prompt('Один из последних просмотренных фильмов?' + i, '');
+    const star = prompt('На сколько оцените его?' + i, '');
+    
+    if (film == '' || star == '') {
+        i=i-1;
+        continue;
+    } else if (film == null || star == null) {
+        i=i-1;
+        continue;
+    } else if (film.length > 50 || star.length > 50) {
+        i=i-1;
+        continue;
+    } else {
+        console.log('не попал никуда');
+    }
+   
+    personalMovieDb.movies[film] = star;
+    
+}
+if (personalMovieDb.count < 10 ) {
+    alert('Просмотрено довольно мало фильмов!', '');    
+} else if (personalMovieDb.count > 10 || personalMovieDb.count < 30){
+    alert('Вы классический зритель!', ''); 
+} else { 
+    alert('Вы киноман!', '');
+}
