e43e29db71a2329a3cc62f3a41035a3a8dfe6f7a
Viktor Ermakov <79091602201@yandex.ru>
2020-11-25 08:56:51 +0300

    повторение за преподавателем

--- script.js
+++ script.js
@@ -68,6 +68,70 @@ function writeYourGenres(count) {
         personalMovieDb.genres[index] = prompt(`Ваш любимый жанр под номером ${index +1}`, '');        
     }
 }
-writeYourGenres(3);
+// writeYourGenres(3);
 
-console.dir(personalMovieDb);
+// function learnJS(lang, callback) {
+//     console.log(`Я учу ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JavaScript', done);
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     maketest: function() {
+//         console.log('test');
+//     }
+// };
+// options.maketest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+// console.log(bg);
+
+// console.log(Object.keys(options).length);
+
+// let counter = 0;
+// for (const key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (const i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);        
+//         }
+//     } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+//     counter ++;
+// }
+// console.log(counter);
+
+// const arr =[2,13,6,8,10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a,b) {
+//     return a-b;
+// }
+
+// arr.forEach(function(index, i, arr) {
+//     console.log(`${i}: ${index} внутри массива ${arr}`);   
+// });
+
+// const str = prompt('','');
+// const product = str.split(', ');
+// console.log(product);
+// product.sort();
+// console.log(product.join('; '));
+let str = "some";
+let strObject = new String(str);
+console.log(typeof(str));
+console.log(typeof(strObject));
